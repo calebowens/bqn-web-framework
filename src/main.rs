720495f3adbe200a7523a7910897410361e0b837
Caleb Owens <caleb@storm-consultancy.com>
2022-12-29 13:18:04 +0000

    Actually working as web framework

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,76 @@
 use std::fs;
+use std::env;
 use cbqn::{eval};
 use cbqn::BQNValue;
+use std::str::FromStr;
 
-fn main() {
-    let file = fs::read_to_string("main.bqn").expect("main.bqn entrypoint expected");
+use std::net::SocketAddr;
 
-    let runtime = eval(&file);
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, StatusCode};
+use tokio::net::TcpListener;
+use hyper::header::{HeaderValue, HeaderName};
 
-    let result = runtime.call1(&eval("{ method ⇐ \"PUT\" }")).to_bqnvalue_vec();
-
-    // Result is expected to be [int, [[string, string]], string]
-    let status_code = result[0].to_f64() as usize;
-    let headers = result[1].to_bqnvalue_vec().iter().map(|item| item.to_bqnvalue_vec().iter().map(BQNValue::to_string).collect::<Vec<String>>()).collect::<Vec<Vec<String>>>();
+fn build_response(result: Vec<BQNValue>) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
+    let status_code = result[0].to_f64() as u16;
+    let headers = result[1]
+        .to_bqnvalue_vec()
+        .iter()
+        .map(|item| item.to_bqnvalue_vec().iter().map(BQNValue::to_string).collect::<Vec<String>>())
+        .collect::<Vec<Vec<String>>>();
     let body = result[2].to_string();
 
-    println!("{} {:?} {}", status_code, headers, body);
+    let mut response = Response::new(Full::new(Bytes::from(body)));
+
+    *response.status_mut() = StatusCode::from_u16(status_code).unwrap();
+
+    for header in headers {
+        response.headers_mut().insert(
+            HeaderName::from_str(&header[0]).unwrap(),
+            HeaderValue::from_str(&header[1]).unwrap()
+        );
+    }
+
+    Ok(response)
+}
+
+async fn run(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
+    let current_directory = env::current_dir().unwrap();
+    let current_directory_str = current_directory.to_str().unwrap();
+
+    let runtime = eval(
+        &(
+            "⟨ \"".to_string() +
+            current_directory_str + "/\", ⟨⟩, ⟨⟩⟩ •BQN •FChars \"" +
+            current_directory_str + "/main.bqn\""
+        )
+    );
+
+    let result = runtime.call1(&eval(&("{ method ⇐ \"".to_string() + req.method().as_str() + "\" }"))).to_bqnvalue_vec();
+
+    build_response(result)
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(run))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
