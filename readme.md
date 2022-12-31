b957ec81477dd03e476e94cecb7bc3f3bf63df4b
Caleb Owens <caleb@storm-consultancy.com>
2022-12-31 16:30:59 +0000

    BQN stuff works!

---
+++ readme.md
@@ -0,0 +1,38 @@
+# BQN Web Framework (name comprised of bacon pun pending)
+
+This project is primarily a way for me to further my understanding of the BQN
+language. BQN is an array programming language, similar to APL or J. My
+understanding is that languages like these are used by scientists, wizards, and
+people with oversized datasets. I however am a web developer so I decided to do
+the web developer thing of building yet another web framework.
+
+## How?
+
+BQN doesn't have any networking built in. Rather than stopping and learning APL
+which does, like a sane person, I decided to crack open the CBQN FFI and have
+the web server portion and have a function which has an input and output
+similar to the ruby rack.
+
+# TODO
+
+- [ ] Parameter parsing
+	- [+] Basic query parameter parsing
+	- [ ] Query parameter parsing supporting arrays and objects
+	- [ ] JSON Body parsing
+- [ ] JSON parsing
+- [ ] JSON stringifying
+- [ ] HTML templating
+- [ ] Routing
+	- [ ] Basic routing
+	- [ ] Argument routes
+	- [ ] Optional routes? // I've never been a fan of optional routes
+	- [ ] Wildcards? // Can be useful at the end of a url. A query param could
+				sufice though
+- [ ] Persistant storage
+	- [ ] Decide between Mongo or Postgres
+	- [ ] Use FFI to interact with either database
+	- [ ] Build an ORM
+		- [ ] Selecting
+		- [ ] Inserting
+		- [ ] Updating
+		- [ ] Deleting
--- src/bqn_request_builder.rs
+++ src/bqn_request_builder.rs
@@ -26,7 +26,7 @@ impl BQNObject {
         let mut out = "{".to_string();
 
         for entry in namespace {
-            out += &(entry.0.to_string() + "⇐" + &entry.1.stringify());
+            out += &(entry.0.to_string() + "⇐" + &entry.1.stringify() + "\n");
         }
 
         out += "}";
@@ -36,7 +36,7 @@ impl BQNObject {
 
     pub fn stringify(&self) -> String {
         match self {
-            BQNObject::String(string) => "\"".to_string() + string + "\"",
+            BQNObject::String(string) => "\"".to_string() + &string.replace("\"", "\"\"") + "\"",
             BQNObject::Number(number) => number.to_string(),
             BQNObject::Array(array) => Self::stringify_array(array),
             BQNObject::Namespace(namespace) => Self::stringify_namespace(namespace),
@@ -44,9 +44,49 @@ impl BQNObject {
     }
 }
 
+trait BQNAble {
+    fn to_bqn_object(&self) -> BQNObject;
+}
+
+impl BQNAble for str {
+    fn to_bqn_object(&self) -> BQNObject {
+        BQNObject::String(self.to_string())
+    }
+}
+
+impl BQNAble for f64 {
+    fn to_bqn_object(&self) -> BQNObject {
+        BQNObject::Number(*self)
+    }
+}
+
+fn build_path(req: &Request<Incoming>) -> BQNObject {
+    BQNObject::Namespace(vec![
+        // ("scheme", req.uri().scheme_str().unwrap_or("").to_bqn_object()),
+        // ("host", req.uri().host().unwrap_or("").to_bqn_object()),
+        // ("port", (req.uri().port_u16().unwrap_or(0) as f64).to_bqn_object()),
+        ("path", req.uri().path().to_bqn_object()),
+        ("query", req.uri().query().unwrap_or("").to_bqn_object())
+    ])
+}
+
+fn build_headers(req: &Request<Incoming>) -> BQNObject {
+    BQNObject::Array(
+        req.headers()
+            .iter()
+            .map(|(header_name, header_value)| BQNObject::Array(vec![
+                header_name.to_string().to_bqn_object(),
+                header_value.to_str().unwrap_or("").to_bqn_object()
+            ]))
+            .collect()
+    )
+}
+
 pub fn build_bqn_request(req: Request<Incoming>) -> BQNValue {
     let object = BQNObject::Namespace(vec![
-        ("method", BQNObject::String(req.method().to_string()))
+        ("method", req.method().to_string().to_bqn_object()),
+        ("uri", build_path(&req)),
+        ("headers", build_headers(&req))
     ]).stringify();
 
     eval(&object)
